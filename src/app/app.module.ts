0df852b955c02c6c2c50d3ca917a50bfacdeff91
Simone Kjær <simonekh@live.dk>
2020-05-08 12:15:15 +0200

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './Component/user-profile/user-profile.component';
+import { MoviesComponent } from './Component/movies/movies.component';
+import { GenreComponent } from './Component/genre/genre.component';
+import { TheaterComponent } from './Component/theater/theater.component';
+import { Routes } from '@angular/router';
+import { ContactComponent } from './component/contact/contact.component';
 
+
+const appRoutes : Routes =[
+  {path : 'Movies', component: MoviesComponent}, //skal matche
+  {path : 'Profile', component: UserProfileComponent}, //skal matche
+  {path : 'Info', component: ContactComponent}, //skal matche
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    MoviesComponent,
+    GenreComponent,
+    TheaterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
